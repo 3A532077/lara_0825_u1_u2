5f652f7ed19a345ff73985db44f3c35cb74ea640
賴律妏 <k0982637445@gamil.com>
2019-01-20 19:16:42 +0800

    練習4 調整Seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,11 +12,24 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         //
+        Post::truncate();
+
         foreach(range(1,20)as$number){
+
+            $total=20;
+            $faker=Faker::create('zh_TW');
             Post::create([
-                'title'=>'title'.$number,
-                'content'=>'content'.$number,
+//                'title'=>'title'.$number,
+//                'content'=>'content'.$number,
+
+                'title'=>$faker->sentence,
+                'content'=>$faker->paragraph,
+                'is_feature'=>rand(0,1),
+                'created_at'=>Carbon::now()->subDays($total-$number),
+                'updated_at'=>Carbon::now()->subDays($total-$number),
+
             ]);
+
         }
     }
 }
