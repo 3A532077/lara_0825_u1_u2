64a5e3be840e05b2b5287b9a4ac12728d37fef5a
賴律妏 <k0982637445@gamil.com>
2019-01-20 18:37:25 +0800

    練習五 刪除資料-destroy方法

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,9 @@ Route::get('/', function () {
     $post->title='saved title';
     $post->content='saved content';
     $post->save();
-    */
+
     $post=\App\Post::find(1);
     $post->delete();
+    */
+    \App\Post::destroy(2);
 });
